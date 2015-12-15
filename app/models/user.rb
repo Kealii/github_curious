2516cbe36c3edb4d66056cdca69645644fceabb1
Michael Wong <michael.brian.wong@gmail.com>
2015-12-14 22:26:04 -0700

    adds image-url to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,9 @@ class User < ActiveRecord::Base
 
   def self.from_omniauth(auth)
     where(uid: auth[:uid]).first_or_create do |new_user|
-      new_user.uid  = auth.uid
-      new_user.name = auth.extra.raw_info.name
+      new_user.uid       = auth.uid
+      new_user.name      = auth.extra.raw_info.name
+      new_user.image_url = auth.extra.raw_info.name
     end
   end
 end
