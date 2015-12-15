f03159c7dcb818fa6cf0ce201594b2914cdfd672
Michael Wong <michael.brian.wong@gmail.com>
2015-12-14 20:34:07 -0700

    adds name to user model from github login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,6 +2,7 @@ class SessionsController < ApplicationController
   def create
     auth = request.env["omniauth.auth"]
     user = User.find_by_uid(auth["uid"]) || User.from_omniauth(auth)
+    byebug
     session[:user_id] = user.id
     redirect_to root_url, notice: "Signed in!"
   end
